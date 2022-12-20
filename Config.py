7435a10f5d5f2c5cfc135f2d717c92315b6f2ae7
dor3Monbotz <118621248+dor3Monbotz@users.noreply.github.com>
2022-12-20 16:44:35 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -22,7 +22,7 @@ else:
     BOT_TOKEN = ""
     DATABASE_URL = ""
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
-    MUST_JOIN = "EmoBotDevolopers"
+    MUST_JOIN = "projectcrown"
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
 
